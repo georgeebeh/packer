280c27bf26ac06fe0c39e5d8713b23aadd75b2a4
George Ebeh <georgeebeh@gmail>
2024-01-12 20:53:55 +0000

    Update .github file

--- packer/packer/install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-sudo apt-get update  
-sudo apt-get install -y nginx
-sudo systemctl enable nginx
-sudo systemctl start nginx
-sudo ufw allow 22/tcp
-sudo ufw allow 80/tcp
-sudo ufw allow 443/tcp
-echo 'y' | sudo ufw enable
